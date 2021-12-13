1a152dcb1260c86de7726126612747048cffcbf8
sebastien reveleau <74558866+Shikadu51@users.noreply.github.com>
2021-12-13 14:24:51 +0100

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,14 +11,18 @@ function verificationemail(){
 
     // Notre condition pour savoir si l'email est valide.
 
-    if(emailpropose.includes('@') && emailpropose.includes('.')){ 
+    if(emailpropose.includes('@') && emailpropose.includes('.')){
+        arobase = emailpropose.indexOf('@');
+        emailnonvalide = emailpropose.substring(arobase);
+        if(emailnonvalide.includes ('.')){ 
          
             message.innerHTML = '<span class="text-success">Votre adresse email est valide</span>';
     }else{
            message.innerHTML = '<span class="text-danger">Votre adresse email est invalide</span>';
-       }    
+       }  
+    }     
             
 }
     // Ajout d'un écouteur d'événement pour vérifier si l'email est valide ou non();
     
-    document.getElementById('email').addEventListener('keydown', verificationemail, false);
+    document.getElementById('email').addEventListener('keyup', verificationemail, false);
